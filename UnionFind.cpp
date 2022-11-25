f42ba84045e1ce03066f69b0bbc67f4f26fcf75e
Nihad Prakarsh <81522007+nihadprakarsh@users.noreply.github.com>
2022-11-25 19:29:28 +0530

    Create UnionFind.cpp

---
+++ UnionFind.cpp
@@ -0,0 +1,28 @@
+Class DSU{
+	vector<int> rank, parent;
+	public:
+	DSU(int n){
+		rank.resize(n+1,0);
+		parent.resize(n+1);
+		for(int i=0; i <= n; ++i)
+		parent[i] = i;
+	}
+
+	int findParent(int node)
+	{
+		if(node==parentNode)
+		return node;
+		return parent[node] = findParent(parent[node]);
+	}
+
+	void Union(int u, int v)
+	{
+		int ulp_u = findParent(u);
+		int ulp_v = findParent(v);
+		if(ulp_u == ulp_v) return;
+		if(rank[ulp_u] < rank[ulp_v])
+		parent[ulp_u] = ulp_v;
+		else
+		parent[ulp_v] = ulp_u;
+	}
+};
