8662fb63367e066c4e4ed97dffb82d4be1eab41f
Nihad Prakarsh <nihadprakarsh@gmail.com>
2025-04-24 00:51:28 +0530

    Update SGTree.cpp

--- SGTree.cpp
+++ SGTree.cpp
@@ -1,16 +1,16 @@
 class SegTree {
 public:
 	int len;
-	vector<int> t;
-	
+	vector<long long int> t;
+
 	SegTree() {}
 	SegTree(int l) {
 		len = l;
 		t.resize(4 * len);
 	}
-	
-	void build(vector<int>& a, int v, int tl, int tr) {
-		if(tl == tr) {
+
+	void build(vector<long long int>& a, int v, int tl, int tr) {
+		if (tl == tr) {
 			t[v] = a[tl];
 			return;
 		}
@@ -19,39 +19,39 @@ public:
 		build(a, 2 * v + 1, tm + 1, tr);
 		t[v] = t[2 * v] + t[2 * v + 1];
 	}
-	
-	int query(int v, int tl, int tr, int l, int r) {
-		if(tl > r || tr < l) return 0;
-		if(l <= tl && tr <= r) return t[v];
-		
+
+	long long int query(int v, int tl, int tr, int l, int r) {
+		if (tl > r || tr < l) return 0;
+		if (l <= tl && tr <= r) return t[v];
+
 		int tm = (tl + tr) / 2;
-		int leftAns = query(2 * v, tl, tm, l, r);
-		int rightAns = query(2 * v + 1, tm + 1, tr, l, r);
+		long long int leftAns = query(2 * v, tl, tm, l, r);
+		long long int rightAns = query(2 * v + 1, tm + 1, tr, l, r);
 		return leftAns + rightAns;
 	}
-	
-	void update(int v, int tl, int tr, int id, int val) {
-		if(tl == id && tr == id) {
+
+	void update(int v, int tl, int tr, int id, long long int val) {
+		if (tl == id && tr == id) {
 			t[v] = val;
 			return;
 		}
-		if(id > tr || id < tl) return;
-		
+		if (id > tr || id < tl) return;
+
 		int tm = (tl + tr) / 2;
 		update(2 * v, tl, tm, id, val);
 		update(2 * v + 1, tm + 1, tr, id, val);
 		t[v] = t[2 * v] + t[2 * v + 1];
 	}
-	
-	void build(vector<int>& a) {
+
+	void build(vector<long long int>& a) {
 		build(a, 1, 0, len - 1);
 	}
-	
-	int query(int l, int r) {
+
+	long long int query(int l, int r) {
 		return query(1, 0, len - 1, l, r);
 	}
-	
-	void update(int id, int val) {
+
+	void update(int id, long long int val) {
 		update(1, 0, len - 1, id, val);
 	}
 };
